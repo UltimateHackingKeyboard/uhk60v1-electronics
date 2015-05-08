e38a6c350139d4771b6a87ceb6a3e0e3fe08ef8f
László Monda <laci@monda.hu>
2015-05-08 08:59:08 +0200

    Add BOM generator script and package.json

---
+++ bom/bom-generator.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var kicadBomGenerator = require('kicad-bom-generator');
+
+var bomFiles = [
+    {
+        name: 'left-main',
+        netlist: __dirname + '/../left-main/left-main.net',
+        components: __dirname + '/../left-main/left-main.cmp'
+    },
+    {
+        name: 'left-main',
+        netlist: __dirname + '/../right-main/right-main.net',
+        components: __dirname + '/../right-main/right-main.cmp'
+    },
+    {
+        name: 'display',
+        netlist: __dirname + '/../display/display.net',
+        components: __dirname + '/../display/display.cmp'
+    }
+];
+
+function generateBomFromFiles(componentFilename, netlistFilename) {
+    var kicadComponents = fs.readFileSync(componentFilename, {encoding:'utf8'});
+    var kicadNetlist = fs.readFileSync(netlistFilename, {encoding:'utf8'});
+    return kicadBomGenerator(kicadComponents, kicadNetlist);
+}
+
+var components = [];
+bomFiles.forEach(function(bomFile) {
+    var newComponents = generateBomFromFiles(bomFile.components, bomFile.netlist);
+    newComponents.forEach(function(newComponent) {
+        newComponent.file = bomFile.name;
+    });
+    components = components.concat(newComponents);
+});
+
+console.log(JSON.stringify(components, null, 4));
