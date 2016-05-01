ff016293394b73c8af188695fedad68b7c549074
László Monda <laci@monda.hu>
2016-05-02 00:28:21 +0200

    Rename connector directory to connectors.

--- gerbers/zip-gerbers.sh
+++ gerbers/zip-gerbers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for directory in connector display left-main right-main; do
+for directory in connectors display left-main right-main; do
     rm -f $directory.zip
     zip --recurse-paths $directory.zip $directory
 done
