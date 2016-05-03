05af0679c7ca0bcc2ccd3288020a9fc8a09afe9b
László Monda <laci@monda.hu>
2016-05-03 14:10:11 +0200

    Zip merged gerbers, too.

--- gerbers/zip-gerbers.sh
+++ gerbers/zip-gerbers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for directory in connectors display left-main right-main; do
+for directory in connectors display left-main right-main merged; do
     rm -f $directory.zip
     zip --recurse-paths $directory.zip $directory
 done
