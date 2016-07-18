0b15579a9cfeeccb5fc9299e52216ef3ced6fa07
László Monda <laci@monda.hu>
2016-07-18 17:53:29 +0200

    Generate CSVs from the report files.

--- bom/bom-generator.js
+++ bom/bom-generator.js
@@ -4,7 +4,13 @@ var fs = require('fs');
 var path = require('path');
 var R = require('ramda');
 
-reportFileToParts = function(reportFilename) {
+var reportFiles = [
+    __dirname + '/../left-main/left-main.rpt',
+    __dirname + '/../right-main/right-main.rpt',
+    __dirname + '/../display/display.rpt'
+];
+
+function reportFileToParts(reportFilename) {
     var reportString = fs.readFileSync(reportFilename, {encoding:'utf8'})
     var boardName = path.basename(reportFilename).replace(/\.[^/.]+$/, '');
 
@@ -43,15 +49,32 @@ reportFileToParts = function(reportFilename) {
         })
 };
 
-var reportFiles = [
-    __dirname + '/../left-main/left-main.rpt',
-    __dirname + '/../right-main/right-main.rpt',
-    __dirname + '/../display/display.rpt'
-];
+function filepathToBoardname(filename) {
+    return path.basename(filename).replace(/\.[^/.]+$/, '');
+}
 
-var parts = reportFileToParts(reportFiles[0]);
-parts = R.reject(R.propEq('attribute', 'virtual'), parts);
-console.log(parts)
+function arrayToCsv(array) {
+    return array.map(function(element) {
+        return '"' + element.replace(/"/g, '\\"') + '"';
+    }).join(',');
+}
+
+function partsToCsvFile(parts, csvFilename) {
+    var csvFileContent = parts
+        .map(function(part) {
+            return [part.reference, part.value, part.footprint, part.attribute];
+        })
+        .map(arrayToCsv).join('\n');
+    fs.writeFileSync(csvFilename, csvFileContent);
+}
+
+reportFiles.forEach(function(reportFile) {
+    var parts = reportFileToParts(reportFile);
+    parts.pop();
+    parts = R.reject(R.propEq('attribute', 'virtual'), parts);
+    partsToCsvFile(parts, filepathToBoardname(reportFile) + '.csv');
+});
+
 return;
 
 var components = [];
