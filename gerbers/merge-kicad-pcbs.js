fa7a0f2b346ef6da0f71ea1115dc3d75a79bd41d
László Monda <laci@monda.hu>
2016-12-03 01:32:44 +0100

    Add script to create a panel out of multiple PCBs.

---
+++ gerbers/merge-kicad-pcbs.js
@@ -0,0 +1,73 @@
+#!/usr/bin/env node
+
+let fs = require('fs');
+
+function parseKicadPcbFile(kicadPcbFilename, getHeader=false, deltaX=0, deltaY=0) {
+
+    function writeResultLines(resultLines) {
+        // Rename all nets to net 1 GND in order to prevent net reference errors.
+        console.log(resultLines.join('\n')
+                               .replace(/\(net [0-9]+ ".+"\)/g, '(net 1 GND)')
+                               .replace(/\(net [0-9]+ [^)]+\)/g, '(net 1 GND)')
+                               .replace(/\(net [0-9]+\)/g, '(net 1)'));
+    }
+
+    let content = fs.readFileSync(kicadPcbFilename, {encoding:'utf8'});
+    let lines = content.split('\n');
+    let resultLines = [];
+    let resultLinesEmptied = false;
+    let moduleStateCounter = 0;
+    let regexp = /\((at|start|end|center|xy) (-?[0-9.]+) (-?[0-9.]+)( -?[0-9.]+)?\)/g;
+
+    for (line of lines) {
+        if (line.match(/^  \(module /)) {
+            if (getHeader) {
+                writeResultLines(resultLines);
+                return;
+            } else if (!resultLinesEmptied) {
+                resultLines = [];
+                resultLinesEmptied = true;
+            }
+            moduleStateCounter = 1;
+        }
+        if (moduleStateCounter && line === '  )') {
+            moduleStateCounter = 0;
+        }
+
+        line = line.replace(regexp, ((match) => {
+            if (moduleStateCounter) {
+                moduleStateCounter++;
+            }
+
+            return match.replace(regexp, (match2, command, x, y, misc) => {
+                let calculatedX = parseFloat(x) + (moduleStateCounter>2 ? 0 : deltaX);
+                let calculatedY = parseFloat(y) + (moduleStateCounter>2 ? 0 : deltaY);
+                return `(${command} ${calculatedX} ${calculatedY}${misc ? ' '+misc : ''})`;
+            })
+        }));
+        resultLines.push(line)
+    };
+
+    // Remove last parenthesis.
+    resultLines.pop();
+    resultLines.pop();
+
+    writeResultLines(resultLines);
+}
+
+function writeHeader(kicadPcbFilename) {
+    return parseKicadPcbFile(kicadPcbFilename, getHeader=true);
+}
+
+function writeContent(kicadPcbFilename, deltaX=0, deltaY=0) {
+    return parseKicadPcbFile(kicadPcbFilename, false, deltaX=deltaX, deltaY=deltaY);
+}
+
+function writeFooter() {
+    console.log(')\n');
+}
+
+writeHeader('../left-main/left-main.kicad_pcb');
+writeContent('../left-main/left-main.kicad_pcb');
+writeContent('../right-main/right-main.kicad_pcb', 150);
+writeFooter();
